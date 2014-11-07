a02e0e051d1e76a4d1e3703a81aa946ab944c2f6
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:47:45 +0100

    Cleanup tests

--- tests/test_stock_relation.py
+++ tests/test_stock_relation.py
@@ -7,30 +7,20 @@ import os
 import sys
 import trytond.tests.test_tryton
 import unittest
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
 
 
 class StockRelationTestCase(unittest.TestCase):
-    '''
-    Test Stock Relation module.
-    '''
+    'Test Stock Relation module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('stock_relation')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('stock_relation')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
